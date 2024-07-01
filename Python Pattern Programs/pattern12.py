39e924746e1ada53b097bd461e8a757ca7b6d8ff
Suba21CS <subashini072004@gmail.com>
2024-07-01 20:13:58 +0530

    Delete Python Pattern Programs/pattern12.py

--- Python Pattern Programs/pattern12.py
+++
@@ -1,9 +0,0 @@
-n = (input("Enter no:"))
-length =len(n)
-for i in range(length):
-    for j in range(length):
-        if i==0 or j==0 or i==length//2 :
-            print(n[i],end=" ")
-        else:
-            print(" ",end=" ")
-    print(" ")
