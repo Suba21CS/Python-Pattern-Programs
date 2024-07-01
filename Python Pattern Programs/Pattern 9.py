cc539dc68204bfff2beda7dff3ae8f8a451aff07
Suba21CS <subashini072004@gmail.com>
2024-07-01 19:06:16 +0530

    Update Pattern 9.py

--- Python Pattern Programs/Pattern 9.py
+++ Python Pattern Programs/Pattern 9.py
@@ -1,7 +1,7 @@
 n = int(input("Enter no:"))
 for i in range(n):
     for j in range(n):
-        if i==0 or j==n-3:
+        if i==0 or j==n//2:
             print("*", end=" ")
         else:
             print(" ", end=" ")
