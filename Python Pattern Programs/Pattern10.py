bc556d8604b4701c0b95d457511743069b11129a
Suba21CS <subashini072004@gmail.com>
2024-07-01 19:17:55 +0530

    Update Pattern10.py

--- Python Pattern Programs/Pattern10.py
+++ Python Pattern Programs/Pattern10.py
@@ -1,7 +1,7 @@
 n = int(input("Enter no:"))
 for i in range(n):
     for j in range(n):
-        if i==0 or i==n-1 or j==0 or i==n-3:
+        if i==0 or i==n-1 or j==0 or i==n//2:
             print("*", end=" ")
         else:
             print(" ",end=" ")
